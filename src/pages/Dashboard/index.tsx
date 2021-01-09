b002c0ed9b4c3323e155a143091752f57320529d
Rodrigo Quintam <rquintam@gmail.com>
2021-01-08 22:49:06 -0300

    Connect API
    Connect to Github API with Axiox

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,54 +1,65 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/app';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
+import Repository from '../Repository';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/52363607?s=400&u=527f5670bdbef6b8c6af4d195cb697e36daf6d8a&v=4"
-            alt="Rodrigo Quintam"
-          />
-          <div>
-            <strong>gostack-challenge-nodejs-fundamentals</strong>
-            <p>Bootcamp GoStack | Challenge 5 - Node.js Fundamentals</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/52363607?s=400&u=527f5670bdbef6b8c6af4d195cb697e36daf6d8a&v=4"
-            alt="Rodrigo Quintam"
-          />
-          <div>
-            <strong>gostack-challenge-nodejs-fundamentals</strong>
-            <p>Bootcamp GoStack | Challenge 5 - Node.js Fundamentals</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/52363607?s=400&u=527f5670bdbef6b8c6af4d195cb697e36daf6d8a&v=4"
-            alt="Rodrigo Quintam"
-          />
-          <div>
-            <strong>gostack-challenge-nodejs-fundamentals</strong>
-            <p>Bootcamp GoStack | Challenge 5 - Node.js Fundamentals</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
