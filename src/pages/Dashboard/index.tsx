e015b08adb2288cc5886f03de3d04952a58f27f6
Rodrigo Quintam <rquintam@gmail.com>
2021-01-08 21:18:04 -0300

    Style Dashboard
    Create and style components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/52363607?s=400&u=527f5670bdbef6b8c6af4d195cb697e36daf6d8a&v=4"
+            alt="Rodrigo Quintam"
+          />
+          <div>
+            <strong>gostack-challenge-nodejs-fundamentals</strong>
+            <p>Bootcamp GoStack | Challenge 5 - Node.js Fundamentals</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/52363607?s=400&u=527f5670bdbef6b8c6af4d195cb697e36daf6d8a&v=4"
+            alt="Rodrigo Quintam"
+          />
+          <div>
+            <strong>gostack-challenge-nodejs-fundamentals</strong>
+            <p>Bootcamp GoStack | Challenge 5 - Node.js Fundamentals</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/52363607?s=400&u=527f5670bdbef6b8c6af4d195cb697e36daf6d8a&v=4"
+            alt="Rodrigo Quintam"
+          />
+          <div>
+            <strong>gostack-challenge-nodejs-fundamentals</strong>
+            <p>Bootcamp GoStack | Challenge 5 - Node.js Fundamentals</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
